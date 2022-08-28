c5a47157a530a0a68202c1ea1d8d02f3325c9ac2
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-28 20:39:42 -0300

    ids and actions

--- TesteJavaFX/src/main/java/com/personal/testejavafx/ViewController.java
+++ TesteJavaFX/src/main/java/com/personal/testejavafx/ViewController.java
@@ -1,4 +1,14 @@
 package com.personal.testejavafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ViewController {
+    @FXML
+    private Button btnMyButton;
+
+    @FXML
+    public void onMyButtonClick() {
+        System.out.println("Olá");
+    }
 }
