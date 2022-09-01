da2706c68d28d3ca5178953a2f51c087d09b2f79
Matheus Dias <matheusdyas007@hotmail.com>
2022-09-01 20:01:44 -0300

    calculadora simples

--- TesteJavaFX/src/main/java/com/personal/testejavafx/ViewController.java
+++ TesteJavaFX/src/main/java/com/personal/testejavafx/ViewController.java
@@ -4,13 +4,38 @@ import com.personal.testejavafx.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
-    @FXML
-    private Button btnMyButton;
 
     @FXML
-    public void onMyButtonClick() {
-        Alerts.showAlert("Alerta!", "Alerta do Matheus", "Esteja alertado", Alert.AlertType.INFORMATION);
+    private TextField txtNumber1;
+
+    @FXML
+    private TextField txtNumber2;
+
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+    public void onBtSumAction() {
+
+        try {
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+
+            double sum = number1 + number2;
+
+            labelResult.setText(String.format("%.2f", sum));
+
+            Alerts.showAlert("Alerta!", null, "Calculo realizado", Alert.AlertType.INFORMATION);
+        }
+        catch (NumberFormatException ex) {
+            Alerts.showAlert("Error!", null, ex.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
