e8fe14986442bef1a8bb9c87f89c3ca45be9fb3e
Matheus Dias <matheusdyas007@hotmail.com>
2022-09-07 13:13:49 -0300

    criando nova view principal

--- javafx-demo-app/src/main/java/com/udemy/javafxdemoapp/Main.java
+++ javafx-demo-app/src/main/java/com/udemy/javafxdemoapp/Main.java
@@ -2,18 +2,20 @@ package com.udemy.javafxdemoapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+        Parent parent = loader.load();
+        Scene mainScene = new Scene(parent);
+        stage.setScene(mainScene);
+        stage.setTitle("FX System");
         stage.show();
     }
 
