57746a2eaff9bd1e88d05262eada5a4f00f6914c
Matheus Dias <matheusdyas007@hotmail.com>
2022-09-09 16:30:28 -0300

    upandro projeto webservice-course

--- javafx-demo-app/src/main/java/com/udemy/javafxdemoapp/controller/MainViewController.java
+++ javafx-demo-app/src/main/java/com/udemy/javafxdemoapp/controller/MainViewController.java
@@ -1,9 +1,18 @@
 package com.udemy.javafxdemoapp.controller;
 
+import com.udemy.javafxdemoapp.Main;
+import com.udemy.javafxdemoapp.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,7 +39,27 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemAboutAction() {
-        System.out.println("onMenuItemAboutAction");
+        loadView("C:\\dev\\udmy-projects\\javafx-demo-app\\src\\main\\resources\\com\\udemy\\javafxdemoapp\\About.fxml");
+    }
+
+    private void loadView(String absoluteName) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            System.out.println(loader.getLocation());
+            VBox newVBox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+        }
+        catch (IOException ex) {
+            Alerts.showAlert("IO Exception", "Error loading view", ex.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
     @Override
