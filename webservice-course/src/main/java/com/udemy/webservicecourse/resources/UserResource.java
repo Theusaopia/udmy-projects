78fb241a6cd4d0fc09d90925e4d06bb9789c8025
Matheus Dias <matheusdyas007@hotmail.com>
2022-09-10 20:50:31 -0300

    camada de servico

--- webservice-course/src/main/java/com/udemy/webservicecourse/resources/UserResource.java
+++ webservice-course/src/main/java/com/udemy/webservicecourse/resources/UserResource.java
@@ -1,19 +1,36 @@
 package com.udemy.webservicecourse.resources;
 
 import com.udemy.webservicecourse.entities.User;
+import com.udemy.webservicecourse.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Matheus", "matheus@email.com", "45998520914", "corinthians");
+    private final UserService service;
 
-        return ResponseEntity.ok(u);
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok(user);
     }
 }
---
+++ webservice-course/src/main/java/com/udemy/webservicecourse/services/UserService.java
@@ -0,0 +1,28 @@
+package com.udemy.webservicecourse.services;
+
+import com.udemy.webservicecourse.entities.User;
+import com.udemy.webservicecourse.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repo.findById(id);
+
+        return obj.get();
+    }
+}
