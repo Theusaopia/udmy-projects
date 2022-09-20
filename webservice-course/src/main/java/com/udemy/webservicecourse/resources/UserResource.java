035a931baa0b771d20c73c02294ff993cd3df1b7
Matheus Dias <matheusdyas007@hotmail.com>
2022-09-20 10:35:38 -0300

    POST de user

--- webservice-course/src/main/java/com/udemy/webservicecourse/resources/UserResource.java
+++ webservice-course/src/main/java/com/udemy/webservicecourse/resources/UserResource.java
@@ -3,11 +3,10 @@ package com.udemy.webservicecourse.resources;
 import com.udemy.webservicecourse.entities.User;
 import com.udemy.webservicecourse.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +31,14 @@ public class UserResource {
         User user = service.findById(id);
 
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(obj);
     }
 }
