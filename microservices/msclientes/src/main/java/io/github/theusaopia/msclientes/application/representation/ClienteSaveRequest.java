d31677a2488e260aeab72fbe9fa24916cd151bac
Theusaopia <matheusdyas007@hotmail.com>
2022-10-17 20:34:46 -0300

    microservices: finalizado implementação do microsserviço de clientes

---
+++ microservices/msclientes/src/main/java/io/github/theusaopia/msclientes/application/representation/ClienteSaveRequest.java
@@ -0,0 +1,15 @@
+package io.github.theusaopia.msclientes.application.representation;
+
+import io.github.theusaopia.msclientes.domain.Cliente;
+import lombok.Data;
+
+@Data
+public class ClienteSaveRequest {
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel() {
+        return new Cliente(cpf, nome, idade);
+    }
+}
