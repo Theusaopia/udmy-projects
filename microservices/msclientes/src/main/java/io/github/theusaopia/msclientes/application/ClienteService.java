a8d0b99891c888ed5c7291d62baf69ef1898b587
Theusaopia <matheusdyas007@hotmail.com>
2022-10-17 15:26:00 -0300

    microservices: inciando implementação do microsserviço de clientes

---
+++ microservices/msclientes/src/main/java/io/github/theusaopia/msclientes/application/ClienteService.java
@@ -0,0 +1,25 @@
+package io.github.theusaopia.msclientes.application;
+
+import io.github.theusaopia.msclientes.domain.Cliente;
+import io.github.theusaopia.msclientes.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf) {
+        return clienteRepository.findByCpf(cpf);
+    }
+}
---
+++ microservices/msclientes/src/main/java/io/github/theusaopia/msclientes/application/ClientesResource.java
@@ -0,0 +1,17 @@
+package io.github.theusaopia.msclientes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clientes")
+public class ClientesResource {
+
+    @GetMapping
+    public String status() {
+        return "ok";
+    }
+
+
+}
