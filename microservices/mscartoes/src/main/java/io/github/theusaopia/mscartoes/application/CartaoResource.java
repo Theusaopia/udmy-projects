cb503cb76e3bb54a59d6f04e27b754fa9bfda22a
Theusaopia <matheusdyas007@hotmail.com>
2022-10-18 15:12:47 -0300

    microservices: implementado microsserviços de cartao parte 1

--- microservices/mscartoes/src/main/java/io/github/theusaopia/mscartoes/application/CartaoResource.java
+++ microservices/mscartoes/src/main/java/io/github/theusaopia/mscartoes/application/CartaoResource.java
@@ -1,15 +1,36 @@
 package io.github.theusaopia.mscartoes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.theusaopia.mscartoes.application.representation.CartaoSaveRequest;
+import io.github.theusaopia.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("cartoes")
+@RequiredArgsConstructor
 public class CartaoResource {
+
+    private final CartaoService service;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request) {
+        Cartao cartao = request.toModel();
+        service.save(cartao);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda) {
+        List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
     }
 }
---
+++ microservices/mscartoes/src/main/java/io/github/theusaopia/mscartoes/domain/BandeiraCartao.java
@@ -0,0 +1,5 @@
+package io.github.theusaopia.mscartoes.domain;
+
+public enum BandeiraCartao {
+    MASTERCARD, VISA
+}
