ba49f7a7c202bb1b8cc0c664713d4af9e48dc0ee
Theusaopia <matheusdyas007@hotmail.com>
2022-10-18 20:17:04 -0300

    microservices: iniciando implementação do microsserviço de avaliação de crédito

---
+++ microservices/msavaliadorcredito/src/main/java/io/github/theusaopia/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -0,0 +1,14 @@
+package io.github.theusaopia.msavaliadorcredito.application;
+
+import io.github.theusaopia.msavaliadorcredito.domain.model.SituacaoCliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AvaliadorCreditoService {
+
+    public SituacaoCliente obterSituacaoCliente(String cpf) {
+        return null;
+    }
+}
