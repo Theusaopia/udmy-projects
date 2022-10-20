95cc21306c72b038d60196991ef1c335afc48af8
Theusaopia <matheusdyas007@hotmail.com>
2022-10-20 07:53:29 -0300

    microservices: buscando cartoes pelo feign

--- microservices/msavaliadorcredito/src/main/java/io/github/theusaopia/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ microservices/msavaliadorcredito/src/main/java/io/github/theusaopia/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,22 +1,29 @@
 package io.github.theusaopia.msavaliadorcredito.application;
 
+import io.github.theusaopia.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.theusaopia.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.theusaopia.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.theusaopia.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.github.theusaopia.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesClient;
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
 
         return SituacaoCliente.builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
     }
 }
