c785bc318a12c3785599775b01058895b19ff8aa
Theusaopia <matheusdyas007@hotmail.com>
2022-10-20 21:09:19 -0300

    microservices: implementando logica de avaliacao de credito

--- microservices/msavaliadorcredito/src/main/java/io/github/theusaopia/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ microservices/msavaliadorcredito/src/main/java/io/github/theusaopia/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -3,9 +3,7 @@ package io.github.theusaopia.msavaliadorcredito.application;
 import feign.FeignException;
 import io.github.theusaopia.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
 import io.github.theusaopia.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
-import io.github.theusaopia.msavaliadorcredito.domain.model.CartaoCliente;
-import io.github.theusaopia.msavaliadorcredito.domain.model.DadosCliente;
-import io.github.theusaopia.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.theusaopia.msavaliadorcredito.domain.model.*;
 import io.github.theusaopia.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.github.theusaopia.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +30,40 @@ public class AvaliadorCreditoService {
                     .cliente(dadosClienteResponse.getBody())
                     .cartoes(cartoesResponse.getBody())
                     .build();
+        }catch (FeignException.FeignClientException e) {
+            int status = e.status();
+            if(HttpStatus.NOT_FOUND.value() == status) {
+                throw new DadosClienteNotFoundException();
+            }
+            throw new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
+    }
+
+    public RetornoAvaliacaoCliente realizarAvaliacao(String cpf, Long renda) throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException{
+        try{
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+            ResponseEntity<List<Cartao>> cartoesResponse = cartoesClient.getCartoesRendaAte(renda);
+
+            List<Cartao> cartoes = cartoesResponse.getBody();
+            var listaCartoesAprovados = cartoes.stream().map(cartao -> {
+
+                DadosCliente dadosCliente = dadosClienteResponse.getBody();
+
+                BigDecimal limiteBasico = cartao.getLimiteBasico();
+                BigDecimal idadeDB = BigDecimal.valueOf(dadosCliente.getIdade());
+                BigDecimal fator = idadeDB.divide(BigDecimal.valueOf(10));
+                BigDecimal limiteAprovado = fator.multiply(limiteBasico);
+
+                CartaoAprovado aprovado = new CartaoAprovado();
+                aprovado.setCartao(cartao.getNome());
+                aprovado.setBandeira(cartao.getBandeira());
+                aprovado.setLimiteAprovado(limiteAprovado);
+
+                return aprovado;
+            }).collect(Collectors.toList());
+
+            return new RetornoAvaliacaoCliente(listaCartoesAprovados);
+
         }catch (FeignException.FeignClientException e) {
             int status = e.status();
             if(HttpStatus.NOT_FOUND.value() == status) {
